d24c09f9ffeecde3602ba8364933dcc9612dec37
sakuya10969 <sakuya.f20010618@gmail.com>
2025-05-28 11:30:18 +0900

    feat: appointment db func

--- app/infrastructure/appointment_repository.py
+++ app/infrastructure/appointment_repository.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, MetaData, insert
+from sqlalchemy.engine import URL
 from app.config.config import get_config
 from app.schemas.schedule import AppointmentRequest
 
@@ -6,24 +7,37 @@ config = get_config()
 
 class AppointmentRepository:
     def __init__(self):
-        self.engine = create_engine(config["DATABASE_URL"], echo=True)
+        self.engine = create_engine(
+            URL.create(
+                "mssql+pyodbc",
+                username="azureuser",
+                password="SRM-password",
+                host="srm-server-k.database.windows.net",
+                port=1433,
+                database="db-SRM-K",
+                query={"driver": "ODBC Driver 18 for SQL Server"}
+            ),
+            echo=True,
+            fast_executemany=True,
+        )
+
         self.meta = MetaData()
         self.meta.reflect(bind=self.engine)
         self.appointments = self.meta.tables["appointments"]
 
     def create_appointment(self, appointment_req: AppointmentRequest):
-        with self.engine.connect() as conn:
-            stmt = insert(self.appointments).values(
-                candidate=appointment_req.candidate,
-                user=appointment_req.user,
-                lastname=appointment_req.lastname,
-                firstname=appointment_req.firstname,
-                company=appointment_req.company,
-                email=appointment_req.email,
-                token=appointment_req.token,
-                candidate_id=appointment_req.candidate_id,
-                stage=appointment_req.stage,
-            )
+        values = {
+            "candidate": appointment_req.candidate,
+            "user": appointment_req.user,
+            "lastname": appointment_req.lastname,
+            "firstname": appointment_req.firstname,
+            "company": appointment_req.company,
+            "email": appointment_req.email,
+            "token": appointment_req.token,
+            "candidate_id": appointment_req.candidate_id,
+            "stage": appointment_req.stage,
+        }
 
+        with self.engine.begin() as conn:
+            stmt = insert(self.appointments).values(values)
             conn.execute(stmt)
-            conn.commit()
