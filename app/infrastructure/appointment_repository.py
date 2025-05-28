488d13b3de56bef2a897c452807e0686b4aff5b6
sakuya10969 <sakuya.f20010618@gmail.com>
2025-05-28 10:54:36 +0900

    fix: db

--- app/infrastructure/appointment_repository.py
+++ app/infrastructure/appointment_repository.py
@@ -1,22 +1,22 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.models.appointment import Appointment
+from sqlalchemy import create_engine, MetaData, insert
 from app.config.config import get_config
+from app.schemas.schedule import AppointmentRequest
 
 config = get_config()
 
 class AppointmentRepository:
     def __init__(self):
         self.engine = create_engine(config["DATABASE_URL"], echo=True)
-        self.Session = sessionmaker(bind=self.engine)
+        self.meta = MetaData()
+        self.meta.reflect(bind=self.engine)
+        self.appointments = self.meta.tables["appointments"]
 
-    def create_appointment(self, appointment: Appointment):
-        session = self.Session()
-        try:
-            session.add(appointment)
-            session.commit()
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
+    def create_appointment(self, appointment_req: AppointmentRequest):
+        with self.engine.connect() as conn:
+            stmt = insert(self.appointments).values(
+                title=appointment_req.title,
+                start_time=appointment_req.start_time,
+                end_time=appointment_req.end_time,
+            )
+            conn.execute(stmt)
+            conn.commit()
