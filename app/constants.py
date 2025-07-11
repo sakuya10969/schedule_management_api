6df101a1c5332df60db6b5def5a6c89a10b5e175
sakuya10969 <sakuya.f20010618@gmail.com>
2025-07-11 13:37:08 +0900

    feat: constants

---
+++ app/constants.py
@@ -0,0 +1,7 @@
+EMPLOYEE_EMAILS = [
+    "a.miyama@intelligentforce.co.jp",
+    "k.ikegami@intelligentforce.co.jp",
+    "m.manda@intelligentforce.co.jp",
+    "m.kawamura@intelligentforce.co.jp",
+    "andante6670@gmail.com",
+]
