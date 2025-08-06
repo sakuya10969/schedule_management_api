14336145ee24e6d7e1750998c7ff9766aa31fd54
sakuya10969 <sakuya.f20010618@gmail.com>
2025-08-06 10:58:22 +0900

    feat: employee directory repository

---
+++ app/infrastructure/employee_directory_repository.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine, MetaData, insert, update, delete, select
+from sqlalchemy.engine import URL
+from app.config.config import get_config
+
+config = get_config()
+
+
+class EmployeeDirectoryRepository:
+    def __init__(self):
+        self.engine = create_engine(
+            URL.create(
+                "mssql+pyodbc",
+                username="azureuser",
+                password="SRM-password",
+                host="srm-server-k.database.windows.net",
+                port=1433,
+                database="db-SRM-K",
+                query={"driver": "ODBC Driver 18 for SQL Server"},
+            ),
+            echo=True,
+            fast_executemany=True,
+        )
+
+        self.meta = MetaData()
+        self.meta.reflect(bind=self.engine)
+        self.employee_directory = self.meta.tables["employee_directory"]
+
+    def get_employee_directory(self):
+        with self.engine.begin() as conn:
+            stmt = select(self.employee_directory)
+            result = conn.execute(stmt)
+            return result.fetchall()
