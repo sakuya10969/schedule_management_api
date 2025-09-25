a59f61906ef4e27bcb19265ff3db993cb7653850
sakuya10969 <sakuya.f20010618@gmail.com>
2025-09-25 15:45:49 +0900

    fix: db settings

--- app/infrastructure/db.py
+++ app/infrastructure/db.py
@@ -9,11 +9,14 @@ engine = create_engine(
         host="srm-server-k.database.windows.net",
         port=1433,
         database="db-SRM-K",
-        query={"driver": "ODBC Driver 18 for SQL Server"},
+        query={
+            "driver": "ODBC Driver 18 for SQL Server",
+            "Encrypt": "yes",
+        },
     ),
     echo=True,
     fast_executemany=True,
+    pool_pre_ping=True,
 )
 
 metadata = MetaData()
-metadata.reflect(bind=engine)
