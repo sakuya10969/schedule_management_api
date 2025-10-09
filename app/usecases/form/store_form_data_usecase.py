c9cbe159de9a69495c9480e66595354d3ef281dc
sakuya10969 <sakuya.f20010618@gmail.com>
2025-10-09 13:05:58 +0900

    refactor: create form usecase class

--- app/usecases/form/store_form_data_usecase.py
+++ app/usecases/form/store_form_data_usecase.py
@@ -7,14 +7,17 @@ from app.infrastructure.az_cosmos import AzCosmosDBClient
 logger = logging.getLogger(__name__)
 
 
-async def store_form_data_usecase(payload: FormData) -> str:
-    """
-    フォームデータを保存し、CosmosDBのIDを返すユースケース
-    """
-    try:
-        cosmos_db_client = AzCosmosDBClient()
-        cosmos_db_id = cosmos_db_client.create_form_data(payload.model_dump())
-        return cosmos_db_id
-    except Exception as e:
-        logger.error(f"フォームデータの保存に失敗しました: {e}")
-        raise HTTPException(status_code=500, detail="Failed to store form data")
+class StoreFormDataUsecase:
+    def __init__(self):
+        self.cosmos_db_client = AzCosmosDBClient()
+
+    async def execute(self, payload: FormData) -> str:
+        """
+        フォームデータを保存し、CosmosDBのIDを返すユースケース
+        """
+        try:
+            cosmos_db_id = self.cosmos_db_client.create_form_data(payload.model_dump())
+            return cosmos_db_id
+        except Exception as e:
+            logger.error(f"フォームデータの保存に失敗しました: {e}")
+            raise HTTPException(status_code=500, detail="Failed to store form data")
